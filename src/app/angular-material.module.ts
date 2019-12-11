1ea2c98c9210fbb68f06330a855a151ae2bf2b35
bnznamco <gabriele.baldi.01@gmail.com>
2019-12-11 12:08:36 +0100

    added material and bootstrap

---
+++ src/app/angular-material.module.ts
@@ -0,0 +1,83 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import {
+   MatButtonModule,
+   MatToolbarModule,
+   MatIconModule,
+   MatBadgeModule,
+   MatSidenavModule,
+   MatListModule,
+   MatGridListModule,
+   MatFormFieldModule,
+   MatInputModule,
+   MatSelectModule,
+   MatRadioModule,
+   MatDatepickerModule,
+   MatNativeDateModule,
+   MatChipsModule,
+   MatTooltipModule,
+   MatTableModule,
+   MatPaginatorModule,
+   MatCardModule,
+   MatDialogModule,
+   MatSnackBarModule,
+   MatProgressSpinnerModule,
+   MatSlideToggleModule
+} from '@angular/material';
+
+@NgModule({
+   imports: [
+      CommonModule,
+      MatButtonModule,
+      MatToolbarModule,
+      MatIconModule,
+      MatSidenavModule,
+      MatBadgeModule,
+      MatListModule,
+      MatGridListModule,
+      MatFormFieldModule,
+      MatInputModule,
+      MatSelectModule,
+      MatRadioModule,
+      MatDatepickerModule,
+      MatNativeDateModule,
+      MatChipsModule,
+      MatTooltipModule,
+      MatTableModule,
+      MatPaginatorModule,
+      MatCardModule,
+      MatDialogModule,
+      MatSnackBarModule,
+      MatProgressSpinnerModule,
+      MatSlideToggleModule
+   ],
+   exports: [
+      MatButtonModule,
+      MatToolbarModule,
+      MatIconModule,
+      MatSidenavModule,
+      MatBadgeModule,
+      MatListModule,
+      MatGridListModule,
+      MatInputModule,
+      MatFormFieldModule,
+      MatSelectModule,
+      MatRadioModule,
+      MatDatepickerModule,
+      MatChipsModule,
+      MatTooltipModule,
+      MatTableModule,
+      MatPaginatorModule,
+      MatCardModule,
+      MatDialogModule,
+      MatSnackBarModule,
+      MatProgressSpinnerModule,
+      MatSlideToggleModule
+   ],
+   providers: [
+      MatDatepickerModule,
+   ]
+})
+
+export class AngularMaterialModule { }
